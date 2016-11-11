e39e7a19abafc2a02a843501c74fe694b6c1bcbc
bensmith <benhsmith13@gmail.com>
2016-11-11 13:33:13 -0700

    and this too

--- src/components/reply.jsx
+++ src/components/reply.jsx
@@ -2,7 +2,24 @@ import React        from 'react';
 
 export default (props) => {
 
-  return <div>
+  const styles = {
+    replies: {
+      backgroundColor: 'grey',
+      padding: '10px',
+      margin: '5px',
+      color: 'white',
+      borderRadius: '5px',
+      textAlign: 'left'
+    },
+    author: {
+      fontStyle: 'italic'
+    }
+  };
 
+  return <div style={styles.replies}>
+    {props.reply.post_text}
+    <div style={styles.author}>
+      - {props.reply.user_name}
+    </div>
   </div>
 }
