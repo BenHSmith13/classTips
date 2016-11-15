02de3bec5ae6f380fa0e6fdc093c572aaedb98b0
bensmith <benhsmith13@gmail.com>
2016-11-15 13:26:59 -0700

    Makes a worse, but more accurate readMe file

---
+++ src/api.example.js
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDzVxP3Wcx5yUd7ZH4CSKnrvl2Mj-_ur7U",
+  authDomain: "classtips-526ad.firebaseapp.com",
+  databaseURL: "https://classtips-526ad.firebaseio.com",
+  storageBucket: "classtips-526ad.appspot.com",
+  messagingSenderId: "100417922122"
+};
