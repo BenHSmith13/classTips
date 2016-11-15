5544bd46bd1fa12063caa1f5520f189fa57f2d9e
bensmith <benhsmith13@gmail.com>
2016-11-15 16:23:42 -0700

    dummy source file

--- src/api.example.js
+++ src/api.example.js
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyDzVxP3Wcx5yUd7ZH4CSKnrvl2Mj-_ur7U",
-  authDomain: "classtips-526ad.firebaseapp.com",
-  databaseURL: "https://classtips-526ad.firebaseio.com",
-  storageBucket: "classtips-526ad.appspot.com",
-  messagingSenderId: "100417922122"
+  apiKey: "YourFirebaseKey",
+  authDomain: "SOMETHING.firebaseapp.com",
+  databaseURL: "https://SOMETHING.firebaseio.com",
+  storageBucket: "SOMETHING.appspot.com",
+  messagingSenderId: "SomeNumber"
 };
